ad8d1e35e55397bdf1b6dd031fc55446f66bf4b6
deciob <decio.battaglia@gmail.com>
2013-11-10 19:23:40 +0000

    saving fluctuating working state before potentially lethal modifications

--- update_enter_exit/main.js
+++ update_enter_exit/main.js
@@ -17,7 +17,6 @@ var xAxis = d3.svg.axis()
 var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
-    //.tickFormat(formatPercent);
 
 var svg = d3.select("#viz").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -26,27 +25,17 @@ var svg = d3.select("#viz").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.csv("data/WUP2011-F11a-30_Largest_Cities.csv", accessor	, function(error, data) {
-  //console.log(data)
   var top_ten = _.filter( data, function (obj) {
     return +obj.rank < 11;
   });
   var data_by_year = _.groupBy( data, function (obj) {
     return obj.year;
   });
-  //console.log(data_by_year);
-
-  //var data_1950 = data.slice(0,10);
-  //var data_2011 = data.slice(10,20);
-  //var current_data = data_1950;
-  //data = data_1950;
-  //x.domain(data.map(function(d) { return d.agglomeration; }));
   y.domain([0, d3.max(data, function(d) {return parseFloat(d.population); })]);
-  //console.log(d3.max(data, function(d) {return parseFloat(d.population); }))
-
+  
   var xAxisDom = svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")");
-      //.call(xAxis);
 
   svg.append("g")
       .attr("class", "y axis")
@@ -63,6 +52,7 @@ d3.csv("data/WUP2011-F11a-30_Largest_Cities.csv", accessor	, function(error, dat
     current_year += 5;
     draw(data_by_year[current_year]);
   });
+  console.log(data_by_year[current_year])
   draw(data_by_year[current_year]);
 
   function agglomeration(d) {
@@ -70,11 +60,9 @@ d3.csv("data/WUP2011-F11a-30_Largest_Cities.csv", accessor	, function(error, dat
   }
 
   function transEnd(d) {
-    //console.log(current_year, +d.rank)
     if (+d.rank === 30 && current_year > 1950) {
       document.getElementById('year-label').innerHTML = current_year;
       current_year += 5;
-      //console.log(current_year, +d.rank)
       if (data_by_year[current_year]) {
         setTimeout(function(){draw(data_by_year[current_year]);},500);
       }
@@ -95,7 +83,6 @@ d3.csv("data/WUP2011-F11a-30_Largest_Cities.csv", accessor	, function(error, dat
       .style("text-anchor", "start");
     var bars = svg.selectAll(".bar")
       .data(data, agglomeration);
-    //bars.on("click", listener);
     bars.enter().append("rect")
       .attr("class", function(d) {
         var active = d.agglomeration == "New York" ? "active" : "";
@@ -115,7 +102,6 @@ d3.csv("data/WUP2011-F11a-30_Largest_Cities.csv", accessor	, function(error, dat
     bars.exit().remove();
   }
   
-
 });
 
 function accessor(d) {
